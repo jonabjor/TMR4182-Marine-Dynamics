f30ac354a68f0a99fcaa3063298077a7e4088b7a
Jonas <jonabjor@stud.ntnu.no>
2020-09-08 21:27:58 +0200

    Added Runge-Kutta and CAA methods

--- Exercise3/const_avg_acc.py
+++ Exercise3/const_avg_acc.py
@@ -1,13 +1,33 @@
-# const_avg_acc(m, k, c, P, t, u0, udot0)
-#
-# This function estimates the response u to a load P using the
-# constant average acceleration method
-#
-# inputs: m, k, c, P, t, u0, udot0
-# returns: the response u (vector of the position of the system)
-def const_avg_acc(m, k, c, P, t, u0, udot0):
-    # Write code using constant avg acc
+import numpy as np
 
-    # Simple harmonic load
-    u = None
+
+def const_avg_acc(m, k, c, P, t, u0, udot0):
+    # Compute ü_0 from initial conditions using the eqn. of motion
+    uacc0 = (P[0] - c * udot0 - k * u0) / m
+
+    # Initialize arrays and add initial conditions
+    u = np.zeros(len(t))
+    u[0] = u0
+
+    udot = np.zeros(len(t))
+    udot[0] = udot0
+
+    uacc = np.zeros(len(t))
+    uacc[0] = uacc0
+
+    # Time vector has constant spacing, therefore we can compute h outside iteration
+    h = t[1] - t[0]
+
+    # Computing the responses using constant average acc method
+    # The left term from compendium eqn (3.80) is constant and therefore computed outside iteration
+    leftterm = ((4/h**2)*m + (2/h)*c + k)
+
+    for i in range(len(t)-1):
+        # Finding the reponse u_i+1
+        u[i+1] = (P[i+1] + m * uacc[i] + ((4 / h) * m + c) * udot[i] +
+                  ((4/h**2) * m + (2/h)*c) * u[i]) / leftterm
+        # Computing ü_i+1
+        uacc[i+1] = (4 / h**2)*(u[i+1] - u[i] - udot[i]*h) - uacc[i]
+        # Computing udot_i+1
+        udot[i+1] = udot[i] + (1 / 2)*(uacc[i] + uacc[i+1])*h
     return u
--- Exercise3/runge_kutta4.py
+++ Exercise3/runge_kutta4.py
@@ -1,4 +1,33 @@
+import numpy as np
+
+
 def runge_kutta4(m, k, c, P, t, u0, udot0):
-    # Write code using constant avg acc
-    u = None
+    # Response vector (displacement and velocity)
+    u = np.zeros(len(t))
+    v = np.zeros(len(t))
+    u[0] = u0
+    v[0] = udot0
+
+    # dx/dt derivative functions to be called from iteration
+    def f1(t, u, v): return v
+    def f2(t, u, v, P): return (1/m)*(P-c*v-k*u)
+
+    # Time step
+    h = t[1]-t[0]
+
+    # Runge-Kutta 4th order method
+    for i in range(len(t)-1):
+        # Computing K1, K2, K3 and K4
+        K0 = np.array([f1(t[i], u[i], v[i]), f2(t[i], u[i], v[i], P[i])])
+        K1 = np.array([f1(t[i] + h/2, u[i] + h/2 * K0[0], v[i] + h/2 * K0[1]),
+                       f2(t[i] + h/2, u[i] + h/2 * K0[0], v[i] + h/2 * K0[1], P[i] + h/2 * P[i])])
+        K2 = np.array([f1(t[i] + h/2, u[i] + h/2 * K1[0], v[i] + h/2 * K1[1]),
+                       f2(t[i] + h/2, u[i] + h/2 * K1[0], v[i] + h/2 * K1[1], P[i] + h/2 * P[i])])
+        K3 = np.array([f1(t[i] + h/2, u[i] + h/2 * K2[0], v[i] + h/2 * K2[1]),
+                       f2(t[i] + h/2, u[i] + h/2 * K2[0], v[i] + h/2 * K2[1], P[i] + h/2 * P[i])])
+
+        # Computing u and v from the weighted average given in the compendium
+        u[i+1] = u[i] + h/6*(K0[0] + 2*K1[0] + 2*K2[0] + K3[0])
+        v[i+1] = v[i] + h/6*(K0[1] + 2*K1[1] + 2*K2[1] + K3[1])
+
     return u
